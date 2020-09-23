2231ff583228f24e2761a3f362206ca3a3af0ecf
djembuka <djembuka@gmail.com>
2020-09-23 18:24:39 +0300

    Shops detail ymap height

--- markup/components/shop.detail/script.js
+++ markup/components/shop.detail/script.js
@@ -11,29 +11,33 @@
             },
             preloadImages: false,
             lazy: true,
-            watchSlidesVisibility: true
-        });
-        ymaps.ready(function() {
-            var shopDetailMap = new ymaps.Map("shopDetailMapID", {
-                center: window.shopDetailMapCoords,
-                zoom: window.shopDetailMapZoom || 9,
-                controls: [ "typeSelector", "zoomControl" ]
-            }, {
-                typeSelectorFloat: "left",
-                zoomControlSize: "small"
-            }), shopDetailPlacemark = new ymaps.Placemark(shopDetailMap.getCenter(), {}, {
-                iconLayout: "default#image",
-                iconImageHref: window.shopDetailMapBalloonHref,
-                iconImageSize: [ 43, 64 ],
-                iconImageOffset: [ -22, -64 ]
-            });
-            shopDetailMap.geoObjects.add(shopDetailPlacemark);
-            shopDetailMap.behaviors.disable("scrollZoom");
-            shopDetailMap.events.add("click", function() {
-                shopDetailMap.behaviors.enable("scrollZoom");
-            });
-            if (window.matchMedia("(max-width: 1024px)").matches) {
-                shopDetailMap.behaviors.disable("drag");
+            watchSlidesVisibility: true,
+            on: {
+                imagesReady: function() {
+                    ymaps.ready(function() {
+                        var shopDetailMap = new ymaps.Map("shopDetailMapID", {
+                            center: window.shopDetailMapCoords,
+                            zoom: window.shopDetailMapZoom || 9,
+                            controls: [ "typeSelector", "zoomControl" ]
+                        }, {
+                            typeSelectorFloat: "left",
+                            zoomControlSize: "small"
+                        }), shopDetailPlacemark = new ymaps.Placemark(shopDetailMap.getCenter(), {}, {
+                            iconLayout: "default#image",
+                            iconImageHref: window.shopDetailMapBalloonHref,
+                            iconImageSize: [ 43, 64 ],
+                            iconImageOffset: [ -22, -64 ]
+                        });
+                        shopDetailMap.geoObjects.add(shopDetailPlacemark);
+                        shopDetailMap.behaviors.disable("scrollZoom");
+                        shopDetailMap.events.add("click", function() {
+                            shopDetailMap.behaviors.enable("scrollZoom");
+                        });
+                        if (window.matchMedia("(max-width: 1024px)").matches) {
+                            shopDetailMap.behaviors.disable("drag");
+                        }
+                    });
+                }
             }
         });
     });
--- source/components/shop.detail/script.js
+++ source/components/shop.detail/script.js
@@ -16,53 +16,55 @@
       },
       preloadImages: false,
       lazy: true,
-      watchSlidesVisibility: true
-      /*on: {
+      watchSlidesVisibility: true,
+      on: {
         init: function () {
-          $( '.b-shops' ).addClass( 'i-swiper-init' );
-        },
-      }*/
-    });
-    
-    //map
-    ymaps.ready( function () {
-      var shopDetailMap = new ymaps.Map( 'shopDetailMapID', {
-        center: window.shopDetailMapCoords,
-        zoom: window.shopDetailMapZoom || 9,
-        controls: ['typeSelector', 'zoomControl']
-      }, {
-          // Зададим опции для элементов управления.
-          typeSelectorFloat: 'left',
-          zoomControlSize: 'small'
-      }),
-      
-      shopDetailPlacemark = new ymaps.Placemark( shopDetailMap.getCenter(), {}, {
-          // Опции.
-          // Необходимо указать данный тип макета.
-          iconLayout: 'default#image',
-          // Своё изображение иконки метки.
-          iconImageHref: window.shopDetailMapBalloonHref,
-          // Размеры метки.
-          iconImageSize: [43, 64],
-          // Смещение левого верхнего угла иконки относительно
-          // её "ножки" (точки привязки).
-          iconImageOffset: [-22, -64]
-      });
+          this.$el.find( 'img' )[0].onload = function() {
+            
+            //map
+            ymaps.ready( function () {
+              var shopDetailMap = new ymaps.Map( 'shopDetailMapID', {
+                center: window.shopDetailMapCoords,
+                zoom: window.shopDetailMapZoom || 9,
+                controls: ['typeSelector', 'zoomControl']
+              }, {
+                  // Зададим опции для элементов управления.
+                  typeSelectorFloat: 'left',
+                  zoomControlSize: 'small'
+              }),
+              
+              shopDetailPlacemark = new ymaps.Placemark( shopDetailMap.getCenter(), {}, {
+                  // Опции.
+                  // Необходимо указать данный тип макета.
+                  iconLayout: 'default#image',
+                  // Своё изображение иконки метки.
+                  iconImageHref: window.shopDetailMapBalloonHref,
+                  // Размеры метки.
+                  iconImageSize: [43, 64],
+                  // Смещение левого верхнего угла иконки относительно
+                  // её "ножки" (точки привязки).
+                  iconImageOffset: [-22, -64]
+              });
 
-      shopDetailMap.geoObjects.add( shopDetailPlacemark );
-      
-      //disable scrollZoom
-      shopDetailMap.behaviors.disable( 'scrollZoom' );
-      shopDetailMap.events.add( 'click', function() {
-        shopDetailMap.behaviors.enable( 'scrollZoom' );
-      });
-        
-      //disable drag for mobile
-      if ( window.matchMedia( '(max-width: 1024px)' ).matches ) {
-        shopDetailMap.behaviors.disable('drag');
+              shopDetailMap.geoObjects.add( shopDetailPlacemark );
+              
+              //disable scrollZoom
+              shopDetailMap.behaviors.disable( 'scrollZoom' );
+              shopDetailMap.events.add( 'click', function() {
+                shopDetailMap.behaviors.enable( 'scrollZoom' );
+              });
+                
+              //disable drag for mobile
+              if ( window.matchMedia( '(max-width: 1024px)' ).matches ) {
+                shopDetailMap.behaviors.disable('drag');
+              }
+              
+            });
+    
+          };
+          //$( '.b-shops' ).addClass( 'i-swiper-init' );
+        },
       }
-      
-      
     });
   
     /*if ( window.BX ) {
