c92000e65fe7a19c30d129093a496a30ba28fd24
djembuka <djembuka@gmail.com>
2022-02-08 17:34:21 +0300

    Top banner new swiper gallery

--- markup/components/top-banner/script.js
+++ markup/components/top-banner/script.js
@@ -1,6 +1,21 @@
 (function($) {
     "use strict";
     $(function() {
-        $(".firstbanerfotorama").fotorama();
+        var swiper = new Swiper("#topSwiper", {
+            pagination: {
+                el: ".swiper-pagination",
+                dynamicBullets: true
+            },
+            slidesPerView: 1,
+            autoplay: {
+                delay: 2e4,
+                disableOnInteraction: false
+            },
+            loop: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            }
+        });
     });
 })(jQuery);
--- source/components/top-banner/script.js
+++ source/components/top-banner/script.js
@@ -1,14 +1,28 @@
-( function($) {
-
+(function ($) {
   'use strict';
-  
-  $( function() {
-    
-    $( '.firstbanerfotorama' ).fotorama();
-  
+
+  $(function () {
+    //$( '.firstbanerfotorama' ).fotorama();
+
+    var swiper = new Swiper('#topSwiper', {
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+      },
+      slidesPerView: 1,
+      autoplay: {
+        delay: 20000,
+        disableOnInteraction: false,
+      },
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
     /*if ( window.BX ) {
       BX.addCustomEvent( "onFrameDataReceived", function () {});
     }*/
   });
-
-}( jQuery ));
+})(jQuery);
