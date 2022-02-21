7587d7339129df4dce9d18b16cbe668deeed2309
djembuka <djembuka@gmail.com>
2022-02-21 13:31:16 +0300

    Shops

--- markup/components/shops/script.js
+++ markup/components/shops/script.js
@@ -1,35 +1,32 @@
-(function($) {
-    "use strict";
-    $(function() {
-        $(".b-shops").each(function() {
-            var $shops = $(this);
-            $shops.find(".b-shops-card__img img").lazyload();
-            var slidesPerView = $shops.data("slidesperview") || 4, spaceBetween = $shops.data("spacebetween") || 30;
-            if (window.matchMedia("(max-width: 400px)").matches) {
-                slidesPerView = 1;
-                spaceBetween = 10;
-            } else if (window.matchMedia("(max-width: 700px)").matches) {
-                slidesPerView = 2;
-                spaceBetween = 10;
-            }
-            var swiper = new Swiper($shops.find(".swiper-container"), {
-                slidesPerView: slidesPerView,
-                spaceBetween: spaceBetween,
-                navigation: {
-                    nextEl: $shops.find(".swiper-button-next"),
-                    prevEl: $shops.find(".swiper-button-prev")
-                },
-                preloadImages: false,
-                lazy: {
-                    loadPrevNext: true
-                },
-                watchSlidesVisibility: true,
-                on: {
-                    init: function() {
-                        $shops.addClass("i-swiper-init");
-                    }
+window.addEventListener("load", function() {
+    document.querySelectorAll(".b-shops").forEach(function(shops) {
+        new Swiper(shops.querySelector(".swiper-container"), {
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev"
+            },
+            preloadImages: false,
+            lazy: {
+                loadPrevNext: true
+            },
+            watchSlidesVisibility: true,
+            on: {
+                init: function() {
+                    shops.classList.add("i-swiper-init");
                 }
-            });
+            },
+            slidesPerView: 1,
+            spaceBetween: 10,
+            breakpoints: {
+                400: {
+                    slidesPerView: 2,
+                    spaceBetween: 10
+                },
+                700: {
+                    slidesPerView: 4,
+                    spaceBetween: 30
+                }
+            }
         });
     });
-})(jQuery);
+});
